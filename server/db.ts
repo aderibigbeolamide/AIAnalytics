2fb1df54b972ed37483aeb31995e5e110c3b25d5
fullstackbeast <aderibigbeolamide56@gmail.com>
2025-07-09 12:52:41 +0100

    adding dotenv

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,10 @@
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
+import 'dotenv/config';
+
 import * as schema from "@shared/schema";
+
 
 neonConfig.webSocketConstructor = ws;
 
